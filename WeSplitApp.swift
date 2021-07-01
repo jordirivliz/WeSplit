e00e66b06e531d61eef658c689a58d9f73d1a3e4
Jorge Rivera <j9rivera@ucsd.edu>
2021-06-30 21:01:01 -0700

    Add files via upload

---
+++ WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jordi Rivera Lizarralde on 29/6/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
